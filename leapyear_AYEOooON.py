5ca736b76a04b5edfff46e601ae523def685e960
AYEOooON <dkdus0402@naver.com>
2022-10-11 20:54:24 +0900

    leapyear and number

--- leapyear_AYEOooON.py
+++ leapyear_AYEOooON.py
@@ -1,13 +1,22 @@
-if year <= 0:
-    print('양의 정수를 입력하세요')
-else:
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 ==0:
-                print(str(year)+"년은 윤년입니다.")
-            else:
-                print(str(year)+"년은 평년입니다.")
-        else:
-            print(str(year)+"년은 윤년입니다.")
+import random 
+
+game = 10
+score = 0
+win = draw = lose = 0
+
+while game > 0:
+    player1 = random.randint(1,6)
+    player2 = random.randint(1,6)
+    if  player2 > player1:
+        score += 2
+        win += 1
+    elif  player2 == player1:
+        score +=1
+        draw += 1
     else:
-        print(str(year)+"년은 평년입니다.")
+        lose += 1
+        
+    game -= 1
+    
+print('내 점수:', score)
+print(str(win)+'승', str(draw)+'무', str(lose)+'패')
--- number_AYEOooON.py
+++ number_AYEOooON.py
@@ -7,9 +7,5 @@ if x > 0:
         print('x는 2의 배수입니다.')
     else:
         print('x는 2의 배수가 아닙니다.')
-        
-    if x % 3 == 0:
-        print('x는 3의 배수 입니다.')
-    else:
-        print('x는 3의 배수가 아닙니다.')
+
         
